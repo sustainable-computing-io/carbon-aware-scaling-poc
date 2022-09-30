1dcffb3ec4bb7099eade07e03d6a83f9913661da
Kaiyi <kaiyiliu21@gmail.com>
2022-09-30 15:15:57 -0400

    Carbon forecast (#6)
    Include abstract base class for Model Pipelines. Currently implemented
    Neural Prophet Model Pipeline. PR also includes datacenter regions of
    major cloud providers. Need to implement it such that each region will
    require its own model. Need to implement XGBooster and ARIMA models. A
    concern is whether enough carbon intensity data can be provided for the
    model training.
    
    Co-authored-by: Yassine EL GHALI <yelghali@gmail.com>

---
+++ forecast/train_pipeline/forecast_models.py
@@ -0,0 +1,69 @@
+from forecast.train_pipeline.GenericCarbonIntensityForecastModel import GenericCarbonIntensityForecastModel
+import pandas as pd
+import xgboost 
+from neuralprophet import NeuralProphet
+from forecast_types import Region
+
+
+class NeuralProphetModel(GenericCarbonIntensityForecastModel):
+    def __init__(self, co2_time_series_dataframe: pd.DataFrame, unique_model_id: str, region: Region):
+        super().__init__(co2_time_series_dataframe, unique_model_id, region)
+        self.model = NeuralProphet(n_lags=3*24, n_forecasts=3*24, changepoints_range=0.90, n_changepoints=30, batch_size=50)
+        self.train_df = None
+
+
+    def preprocess_co2_time_series_df(self) -> None:
+        if self.co2_time_series_dataframe.empty:
+            raise Exception("dataframe is empty")
+        clean_df = pd.DataFrame(columns=['ds', 'y'])
+        clean_df['ds'] = pd.DatetimeIndex(self.co2_time_series_dataframe['datetime'])
+        clean_df['y'] = pd.DatetimeIndex(self.co2_time_series_dataframe['carbon_intensity'])
+        self.train_df = clean_df
+        
+
+    def train(self) -> None:
+        if self.train_df is None:
+            raise Exception("no trained dataframe")
+        self.model.fit(df=self.train_df, freq="60min")
+
+
+    def save_current_model(self) -> str:
+        #default save
+        pass
+
+    
+    def load_existing_model(self, filepath: str) -> None:
+        pass
+
+
+    def forecast(self, steps_into_future: str) -> pd.DataFrame:
+        future = self.model.make_future_dataframe(self.train_df,periods=steps_into_future, n_historic_predictions=True)
+        forecast = self.model.predict(future)
+        self.model.plot(forecast)
+        return forecast.tail(steps_into_future)
+
+
+class XGBoostRegressorModel(GenericCarbonIntensityForecastModel):
+    def __init__(self, co2_time_series_dataframe: pd.DataFrame, region: Region):
+        super().__init__(co2_time_series_dataframe, region)
+
+
+    def preprocess_co2_time_series_df(self) -> None:
+        return super().preprocess_co2_time_series_df()
+
+
+    def train(self) -> None:
+        return super().train()
+
+
+    def save_current_model(self) -> str:
+        return super().save_current_model()
+
+
+    def load_existing_model(self, filepath: str):
+        return super().load_existing_model(filepath)
+
+
+    def forecast(self, steps_into_future: str) -> pd.DataFrame:
+        return super().forecast(steps_into_future)
+    
---
+++ forecast/train_pipeline/forecast_types.py
@@ -0,0 +1,41 @@
+from enum import Enum
+
+class Zone(Enum):
+    pass    
+
+class Region(Enum):
+    Taiwan_Datacenter = 1
+    Hong_Kong_Datacenter = 2
+    Mainland_China_Datacenter = 3
+    Japan_Datacenter = 4
+    Korea_Datacenter = 5
+    India_Datacenter = 6
+    Singapore_Datacenter = 7
+    Indonesia_Datacenter = 8
+    Australia_Datacenter = 9
+    Great_Britain_Datacenter = 10
+    Germany_Datacenter = 11
+    Madrid_Spain_Datacenter = 12
+    Finland_Datacenter = 13
+    Poland_Datacenter = 14
+    Belgium_Datacenter = 15
+    Netherlends_Datacenter = 16
+    France_Datacenter = 17
+    Italy_Datacenter = 18
+    Switzerland_Datacenter = 19
+    Canada_Ontario_Datacenter = 20
+    Canada_Montreal_Datacenter = 21
+
+
+class Step_Type(Enum):
+    Hour = 1
+    Day = 2
+    Half_Hour = 3
+    Month = 4
+    Year = 5
+
+
+zones_to_region = {
+
+
+}
